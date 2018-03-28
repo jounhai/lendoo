40d54c8fd775301308e4b48bd043d49fa7ebe03f
吴志海 <570211052@qq.com>
2018-03-28 22:29:05 +0800

    change hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['pre_system'] = function(){
+	//参数一次为appid， appkey， masterkey
+	Client::initialize("AhHpWi6q6nu3JvtkjhnO2qXM-gzGzoHsz","1WePIUx9gu2QPbyGbYwD3oW6","gabOdlLn74iFlwjUJhjuhVD0");
+	Client::useMasterKey(true);
+	Client::setStorage(new CookieStorage());
+	$engine = new CIEngine();
+
+	$engine();
+};
